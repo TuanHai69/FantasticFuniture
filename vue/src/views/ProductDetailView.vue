c85a49bd32530c0182969c17f16d0e71cfb73113
TuanHai69 <akasasaisuca@gmail.com>
2024-11-08 15:23:11 +0700

    1

--- vue/src/views/ProductDetailView.vue
+++ vue/src/views/ProductDetailView.vue
@@ -2,12 +2,12 @@
     <AppHeader />
     <div class="shopcardbg">
         <div class="container-fluid ">
-            <div class="page row" v-if="!editingProductId">
+            <div class="page row" v-if="!editingProductId && !updatingPriceProductId">
                 <div class="offset-1 col-md-10">
                     <div class="d-flex justify-content-center mt-3">
-                        <ProductHead :id=id @edit-product="handleEditProduct" />
+                        <ProductHead :id=id @edit-product="handleEditProduct" @update-price="handleUpdatePrice" />
                     </div>
-                    <CommandProductCard v-if="this.product.storeid" :productid="id" :storeid="this.product.storeid"  />
+                    <CommandProductCard v-if="this.product.storeid" :productid="id" :storeid="this.product.storeid" />
                 </div>
 
                 <ShopBodyCard :storeid="this.product.storeid" :editstate="'hide'" v-if="this.product.storeid" />
@@ -16,6 +16,9 @@
             <div class="page row" v-else-if="editingProductId">
                 <ProductForm :storeid="id" :productid="editingProductId" @cancel="handleCancelEditProduct"
                     @update-product="handleUpdateProductSubmit" />
+            </div>
+            <div class="page row" v-else-if="updatingPriceProductId">
+                <PriceForm :productid="updatingPriceProductId" @cancel="handleCancelUpdatePrice" />
             </div>
         </div>
     </div>
@@ -29,6 +32,8 @@ import ShopBodyCard from "@/components/ShopBodyCard.vue";
 import ProductService from "@/services/product.service";
 import ProductForm from "@/components/ProductForm.vue";
 import CommandProductCard from "@/components/CommandProductCard.vue";
+import PriceForm from "@/components/PriceForm.vue";
+
 export default {
     props: {
         id: {
@@ -40,6 +45,7 @@ export default {
         return {
             product: {},
             editingProductId: null,
+            updatingPriceProductId: null,
         };
     },
     components: {
@@ -49,6 +55,7 @@ export default {
         ShopBodyCard,
         ProductForm,
         CommandProductCard,
+        PriceForm,
     },
     watch: {
         id: {
@@ -76,12 +83,17 @@ export default {
         handleCancelEditProduct() {
             this.editingProductId = null;
         },
+        handleUpdatePrice(productId) {
+            this.updatingPriceProductId = productId;
+        },
+        handleCancelUpdatePrice() {
+            this.updatingPriceProductId = null;
+        },
         async handleUpdateProductSubmit(product) {
             try {
                 await ProductService.update(product._id, product);
                 alert('Cập nhật thông tin sản phẩm thành công!');
                 this.editingProductId = null;
-                // Optionally, refresh the product list or perform other actions
             } catch (error) {
                 console.error('Error updating product:', error);
                 alert('Có lỗi xảy ra khi cập nhật thông tin sản phẩm.');
